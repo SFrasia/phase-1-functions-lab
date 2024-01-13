a677f43a1e5187281c0999e191784f85295f0bc8
SFrasia <108404961+SFrasia@users.noreply.github.com>
2024-01-13 10:40:38 +0300

    Changes

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+
+//returns the number of blocks given a value
+// Code your solution in this file!
+// Calculate blocks
+function distanceFromHqInBlocks(location) {
+  if (location <= 42) {
+    return 42 - location;
+  } else {
+    return location - 42;
+  }
+}
+
+// Distance in feet from HQ
+function distanceFromHqInFeet(location) {
+  return 264 * distanceFromHqInBlocks(location);
+}
+
+// Distance travelled in feet
+function distanceTravelledInFeet(start, stop) {
+  if (start >= stop) {
+    return 264 * (start - stop);
+  } else {
+    return 264 * (stop - start);
+  }
+}
+
+// fare price
+function calculatesFarePrice(start, stop) {
+  let feet = distanceTravelledInFeet(start, stop);
+  if (feet <= 400) {
+    return 0;
+  } else if (feet > 400 && feet <= 2000) {
+    return (feet - 400) * 0.02;
+  } else if (feet > 2000 && feet < 2500) {
+    return 25;
+  } else if (feet > 2500) {
+    return "cannot travel that far";
+  }
+}
